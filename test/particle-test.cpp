fa3f27a35229f43daf07c50ca350fc35ea500e95
Zoltan Kosa <zoltan.kosa.cfd@gmail.com>
2020-03-22 22:35:06 +0100

    prepare for terminal velocity testruns with variable particle diameter, update parallel touching collision test

--- test/particle-test.cpp
+++ test/particle-test.cpp
@@ -1,5 +1,6 @@
 #include "particle.h"
-#define BOOST_TEST_TOOLS_UNDER_DEBUGGER
+
+//#define BOOST_TEST_TOOLS_UNDER_DEBUGGER
 #define BOOST_TEST_DYN_LINK
 #define BOOST_TEST_MODULE particle-TEST
 #include <boost/test/unit_test.hpp>
@@ -24,7 +25,6 @@ BOOST_AUTO_TEST_CASE( construction_test )
 
 	p1.setV(2*vel);
 	BOOST_REQUIRE_EQUAL( p1.getV(), 2*vel );
-
 }
 
 BOOST_AUTO_TEST_CASE( collision_touching_test )
@@ -45,7 +45,6 @@ BOOST_AUTO_TEST_CASE( collision_touching_test )
 */
 	BOOST_REQUIRE_EQUAL( p1.getPos(), -1*p2.getPos() );
 	BOOST_REQUIRE_EQUAL( p1.getV(), -1*p2.getV() );
-
 }
 
 BOOST_AUTO_TEST_CASE( collision_overlapping_test )
@@ -62,7 +61,6 @@ BOOST_AUTO_TEST_CASE( collision_overlapping_test )
 
 	BOOST_REQUIRE_EQUAL( p1.getPos(), -1*p2.getPos() );
 	BOOST_REQUIRE_EQUAL( p1.getV(), -1*p2.getV() );
-
 }
 
 BOOST_AUTO_TEST_CASE( collision_distant_test )
@@ -79,29 +77,28 @@ BOOST_AUTO_TEST_CASE( collision_distant_test )
 
 	BOOST_REQUIRE_EQUAL( p1.getPos(), -1*p2.getPos() );
 	BOOST_REQUIRE_EQUAL( p1.getV(), -1*p2.getV() );
-
 }
 
 
 BOOST_AUTO_TEST_CASE( collision_touching_parallel_test )
 {
+	// no friction --> no change in velo/pos
 	float r = 0.005;
 	Vec3d pos(0,0,0);
 	Vec3d vel(0,1,0);
 
-	Particle p1(pos - r*Vec3d::i, vel, r);
-	Particle p2(pos + r*Vec3d::i, vel, r);
+	Vec3d pos1 = pos - r*Vec3d::i;
+	Vec3d pos2 = pos + r*Vec3d::i;
+
+	Particle p1(pos1, vel, r);
+	Particle p2(pos2, vel, r);
 
 	p1.collideToParticle(p2);
-/*
-	p1.getV().print();
-	p2.getV().print();
-	p1.getPos().print();
-	p2.getPos().print();
-*/
-	BOOST_REQUIRE_EQUAL( p1.getPos(), -1*p2.getPos() );
-	//BOOST_REQUIRE_EQUAL( p1.getV(), -1*p2.getV() ); // fails
 
+	BOOST_REQUIRE_EQUAL( p1.getPos(), pos1 );
+	BOOST_REQUIRE_EQUAL( p2.getPos(), pos2 );
+	BOOST_REQUIRE_EQUAL( p1.getV(), vel );
+	BOOST_REQUIRE_EQUAL( p2.getV(), vel );
 }
 
 BOOST_AUTO_TEST_CASE( no_drag_fall_test )
@@ -113,7 +110,7 @@ BOOST_AUTO_TEST_CASE( no_drag_fall_test )
 	Vec3d vel(0,0,0);
 
 	Particle p(pos, vel, r);
-	Particle::setCd(0);
+	Particle::setCd(0.0);
 
 	float elapsed_time = 0;
 	do {
@@ -127,12 +124,13 @@ BOOST_AUTO_TEST_CASE( no_drag_fall_test )
 
 	float tolerance = 0.001; // [%]
 	BOOST_REQUIRE_CLOSE( calculated_time, elapsed_time, tolerance );
-
 }
 
 static const boost::array< float, 6 > Cd_data{0.01, 0.1, 0.5, 5.0, 25.0, 100.0}; // it would fail with 0, because there the terminal velocity is infinite
+//static const boost::array< float, 5 > r_data{0.001, 0.005, 0.025, 0.1, 0.25};
 
 BOOST_DATA_TEST_CASE( terminal_velocity_test, Cd_data, Cd )
+//BOOST_DATA_TEST_CASE( terminal_velocity_test, r_data, R )
 {
 	float time_step = 0.001; // [s]
 	float r = 0.005;
@@ -154,8 +152,7 @@ BOOST_DATA_TEST_CASE( terminal_velocity_test, Cd_data, Cd )
 
 	float simulated_terminal_velocity = abs(p.getV());
 
-	//std::cout << calculated_terminal_velocity << "  " << simulated_terminal_velocity << std::endl;
-	float tolerance = 0.1; // [%]
+	//std::cout << calculated_terminal_velocity << std::endl;
+	float tolerance = 0.05; // [%]
 	BOOST_REQUIRE_CLOSE( calculated_terminal_velocity, simulated_terminal_velocity, tolerance );
-
 }
