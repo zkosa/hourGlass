cbd8d1dc85f90e1bef59acdf84a0a4656f66dba3
Zoltan Kosa <zoltan.kosa.cfd@gmail.com>
2020-03-25 01:26:58 +0100

    extend test in scene for the drifting middle particle, and create a simpler one too

--- test/scene-test.cpp
+++ test/scene-test.cpp
@@ -1,5 +1,6 @@
 #include "scene.h"
 
+//#define BOOST_TEST_TOOLS_UNDER_DEBUGGER
 #define BOOST_TEST_DYN_LINK
 #define BOOST_TEST_MODULE scene-TEST
 #include <boost/test/unit_test.hpp>
@@ -10,7 +11,7 @@ void print(const std::vector<Particle>& particles) {
 	int counter = 0;
 	for (const auto& p : particles) {
 		std::cout << counter++ << ": ";
-		p.getV() .print();
+		p.getV().print();
 	}
 }
 
@@ -48,8 +49,12 @@ BOOST_AUTO_TEST_CASE( scene_cell_external_test )
 	}
 }
 
-BOOST_AUTO_TEST_CASE( scene_add_particles_test )
+BOOST_AUTO_TEST_CASE( scene_drag_test )
 {
+	// The middle particle drifted away when the static force field
+	// was overwritten by the acceleration of the drag of the
+	// left particle
+
 	Scene scene;
 
 	Scene *scene_ptr = &scene; // TODO: check
@@ -57,7 +62,45 @@ BOOST_AUTO_TEST_CASE( scene_add_particles_test )
 	Cell::connectScene(scene_ptr);
 
 	scene.applyDefaults();
-	Particle::setCd(0.0f); // easier checking without drag
+	//Particle::setCd(0.0f);
+
+	scene.setGeometry(test);
+	scene.createGeometry(test);
+	scene.setNumberOfParticles(3);
+	scene.addParticles(scene.getNumberOfParticles());
+
+	auto& p_left = scene.getParticles()[0];
+	auto& p_middle = scene.getParticles()[1];
+	auto& p_right = scene.getParticles()[2];
+
+	scene.setVeloThreeParticlesTest();
+	p_left.setV(Vec3d(10, 0, 0));
+	p_right.setV(Vec3d(-10, 0, 0));
+	BOOST_REQUIRE_EQUAL( p_middle.getV().x , 0 );
+
+	scene.populateCells();
+	scene.advance();
+	BOOST_REQUIRE_EQUAL( p_middle.getV().x , 0 );
+
+	scene.calculatePhysics();
+	BOOST_REQUIRE_EQUAL( p_middle.getV().x , 0 );
+}
+
+BOOST_AUTO_TEST_CASE( scene_three_particles_test )
+{
+	// The middle particle drifted away when the static force field
+	// was overwritten by the acceleration of the drag of the
+	// left particle
+
+	Scene scene;
+
+	Scene *scene_ptr = &scene; // TODO: check
+	Particle::connectScene(scene_ptr);
+	Cell::connectScene(scene_ptr);
+
+	scene.applyDefaults();
+	float Cd = 0.5f;
+	Particle::setCd(Cd);
 	scene.setGeometry(test);
 	scene.createGeometry(test);
 	scene.setNumberOfParticles(3);
@@ -65,7 +108,10 @@ BOOST_AUTO_TEST_CASE( scene_add_particles_test )
 	scene.setVeloThreeParticlesTest();
 	scene.populateCells();
 
-	std::cout << "after creation:" << std::endl;
+	auto& p_left = scene.getParticles()[0];
+	auto& p_middle = scene.getParticles()[1];
+	auto& p_right = scene.getParticles()[2];
+
 	print(scene.getParticles());
 
 	//MainWindow::run()
@@ -73,19 +119,21 @@ BOOST_AUTO_TEST_CASE( scene_add_particles_test )
 	scene.resolveConstraintsOnInitCells(5);
 	scene.populateCells();
 
-	std::cout << "after start:" << std::endl;
-	print(scene.getParticles());
+	BOOST_REQUIRE_EQUAL( p_middle.getV().x , 0 );
+	if (Cd < SMALL) {
+		BOOST_REQUIRE_EQUAL( p_left.getV().x , 10 );
+		BOOST_REQUIRE_EQUAL( p_right.getV().x , -10 );
+	}
 
 	scene.calculatePhysics();
-	std::cout << "after one loop:" << std::endl;
-	print(scene.getParticles());
-
-	scene.calculatePhysics();
-	std::cout << "after one more loop:" << std::endl;
-	print(scene.getParticles());
+	BOOST_REQUIRE_EQUAL( p_middle.getV().x , 0 );
+	if (Cd < SMALL) {
+		BOOST_REQUIRE_EQUAL( p_left.getV().x , 10 );
+		BOOST_REQUIRE_EQUAL( p_right.getV().x , -10 );
+	}
 
 	for (auto p : scene.getParticles()) {
-		std::cout << scene.getParticles()[0].getID() << "\t";
+		std::cout << p.getID() << "\t";
 	}
 	std::cout << std::endl;
 }
