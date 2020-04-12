2f59972d2d32afc4a754f0311f7044779624397c
Zoltan Kosa <zoltan.kosa.cfd@gmail.com>
2020-04-12 23:06:53 +0200

    create devtools.h for commonly used macros

---
+++ include/devtools.h
@@ -0,0 +1,11 @@
+#ifndef INCLUDE_DEVTOOLS_H_
+#define INCLUDE_DEVTOOLS_H_
+
+#include <iostream>
+
+/* Tools to be used in multiple files. */
+
+#define watch(x) std::cout << (#x) << "= " << (x) << std::endl
+
+
+#endif /* INCLUDE_DEVTOOLS_H_ */
