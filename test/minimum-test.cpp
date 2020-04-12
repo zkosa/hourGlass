b5432f89b1fbb633ebb6c8af3d3a43575a010e89
Zoltan Kosa <zoltan.kosa.cfd@gmail.com>
2020-04-13 01:52:14 +0200

    check the minimum search with different starting values

--- test/minimum-test.cpp
+++ test/minimum-test.cpp
@@ -1,14 +1,17 @@
 #include "devtools.h"
 #include "minimum.h"
 
-//#define BOOST_TEST_TOOLS_UNDER_DEBUGGER
+//#define BOOST_TEST_TOOLS_UNDER_DEBUGGER // it can deactivate the tolerance!!!
 #define BOOST_TEST_DYN_LINK
 #define BOOST_TEST_MODULE minimum-TEST
 #include <boost/test/unit_test.hpp>
+#include <boost/test/data/test_case.hpp>
+#include <boost/array.hpp>
 
 float vertex_height = 0.2;
 float X_offset = 0.1;
 
+// TODO: test with sine function too
 float parabola(float X) {
 	return (X - X_offset)*(X - X_offset) + vertex_height;
 }
@@ -18,7 +21,11 @@ float distance2(float X, float X0, float R0) {
 	return (X - X0) * (X - X0) + (parabola(X) - R0) * (parabola(X) - R0);
 }
 
-BOOST_AUTO_TEST_CASE( parabola_minimum_test )
+// same result is expected with various starting values
+static const boost::array< float, 9 > guess_data{
+	-1.0f, -0.1f, 0.0f, 0.005f, X_offset - 1e-5f, X_offset, X_offset + 1e-6f, 0.5f, 1.0f };
+
+BOOST_DATA_TEST_CASE( parabola_minimum_test, guess_data, guess_ )
 {
 	// find the vertex of the parabola
 
@@ -27,14 +34,20 @@ BOOST_AUTO_TEST_CASE( parabola_minimum_test )
 
 	Minimum curve_minimum(parabola);
 
-	float tolerance = Minimum::getTolerance();
-	float guess = 0.005;
+	//auto tolerance = boost::test_tools::tolerance( float(3.0f * Minimum::getTolerance()) );
+	auto tolerance = boost::test_tools::tolerance( float(3 * 1e-05f) );
+	// changing the tolerance does not effect the result, only the number of iterations. hmmm...
+	//curve_minimum.setTolerance(1e-9f);
+	float guess = guess_;
 	float root = curve_minimum.findRoot(guess);
 	float truth = X_offset;
 
-	BOOST_REQUIRE( std::abs(root - truth) < tolerance );
+	//watch( (root - truth) / truth );
+	//watch( curve_minimum.getPerformedIterations() );
+
+	BOOST_TEST_REQUIRE( root == truth, tolerance );
 
 	// check repeated execution:
 	root = curve_minimum.findRoot(guess);
-	BOOST_REQUIRE( std::abs(root - truth) < tolerance );
+	BOOST_TEST_REQUIRE( root == truth, tolerance );
 }
