76894742aee30e52f39f1eaee15e29ac637f6e0a
Zoltan Kosa <zoltan.kosa.cfd@gmail.com>
2020-06-29 02:32:55 +0200

    adding simple cuda test (print version info), using workaround in cmake because of superfluous -fPIC to nvcc when Qt5:Core is linked

---
+++ test/cuda-test.cpp
@@ -0,0 +1,10 @@
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE cuda-TEST
+#include <boost/test/unit_test.hpp>
+#include "test.cuh"
+
+
+BOOST_AUTO_TEST_CASE( cuda_test )
+{
+	printCudaVersion();
+}
