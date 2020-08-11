a609334f42db15051ee59627964915318c03185e
Zoltan Kosa <zoltan.kosa.cfd@gmail.com>
2020-08-12 01:33:32 +0200

    add TODO

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "mainwindow.h"
 #include <QApplication>
 
+// TODO: add command line arguments
 int main(int argc, char *argv[]) {
 	QApplication application(argc, argv);
 	MainWindow window;
