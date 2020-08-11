75a5c74daf17ee50edfc8478416fdd8444b28b5e
Zoltan Kosa <zoltan.kosa.cfd@gmail.com>
2020-08-12 01:22:39 +0200

    benchmark the hourGlass

--- test/benchmark.cpp
+++ test/benchmark.cpp
@@ -1,18 +1,24 @@
 #include <benchmark/benchmark.h>
 
-static void BM_StringCreation(benchmark::State& state) {
-  for (auto _ : state)
-    std::string empty_string;
+#include "mainwindow.h"
+#include <QApplication>
+
+
+void benchMark_hourGlass(benchmark::State& state) {
+	for (auto _ : state) {
+		// argv must be composed of non const character arrays
+		char arg0[] = "hourGlass_benchmark";
+		char *argv[] = { &arg0[0], NULL };
+		int   argc = (int)(sizeof(argv) / sizeof(argv[0])) - 1;
+
+		QApplication application(argc, argv);
+		MainWindow window;
+		window.show();
+
+		application.exec();
+	}
 }
 // Register the function as a benchmark
-BENCHMARK(BM_StringCreation);
-
-// Define another benchmark
-static void BM_StringCopy(benchmark::State& state) {
-  std::string x = "hello";
-  for (auto _ : state)
-    std::string copy(x);
-}
-BENCHMARK(BM_StringCopy);
+BENCHMARK(benchMark_hourGlass);
 
 BENCHMARK_MAIN();
