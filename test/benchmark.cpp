70bf354a4a8f0a044f625652088081474725d307
zkosa <zoltan.kosa.cfd@gmail.com>
2020-08-15 23:04:49 +0200

    add physics loop benchmark without the GUI

--- test/benchmark.cpp
+++ test/benchmark.cpp
@@ -19,6 +19,36 @@ void benchMark_hourGlass(benchmark::State& state) {
 	}
 }
 // Register the function as a benchmark
-BENCHMARK(benchMark_hourGlass);
+//BENCHMARK(benchMark_hourGlass);
+
+void benchMark_hourGlass_calculatePhysics(benchmark::State& state) {
+	for (auto _ : state) {
+		Scene scene;
+
+		// TODO: wrap these into a single command:
+		// TODO: create and connect a MainWindow too, or eliminate it
+		Scene *scene_ptr = &scene;
+		Particle::connectScene(scene_ptr);
+		Cell::connectScene(scene_ptr);
+		scene.setBenchmarkMode(true);
+
+		scene.applyDefaults();
+		int N = scene.getDefaults().number_of_particles;
+		scene.setNumberOfParticles(N);
+		scene.addParticles(scene.getNumberOfParticles());
+
+		scene.createCells();
+		scene.populateCells();
+		scene.resolveConstraintsOnInitCells(5);
+		scene.populateCells();
+
+		scene.setRunning();
+		while ( scene.isRunning() ) {
+			scene.calculatePhysics();
+		}
+	}
+}
+
+BENCHMARK(benchMark_hourGlass_calculatePhysics);
 
 BENCHMARK_MAIN();
