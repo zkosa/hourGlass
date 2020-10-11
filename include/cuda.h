f627dd7c696007710cd2bc25e680f82eec7c6356
zkosa <zoltan.kosa.cfd@gmail.com>
2020-10-12 00:59:15 +0200

    adding CUDA error check macro

--- include/cuda.h
+++ include/cuda.h
@@ -3,5 +3,18 @@
 
 #include <cuda_runtime_api.h> // just for proper indexing, nvcc includes it anyhow
 #define CUDA_HOSTDEV __host__ __device__
+#define CHECK_CUDA(cuda_call) { cudaAssert((cuda_call), __FILE__, __LINE__); }
+
+inline void cudaAssert(cudaError_t error_code, const char *file, int line, bool abort=true)
+{
+	if (error_code != cudaSuccess) {
+
+		fprintf(stderr,"cudaAssert: %s %s %d\n", cudaGetErrorString(error_code), file, line);
+
+		if (abort) {
+			exit(error_code);
+		}
+	}
+}
 
 #endif /* CUDA_H_ */
