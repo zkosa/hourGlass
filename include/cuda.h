5ad7965ecef1d356288a3595e94d5721dc6d3d53
zkosa <zoltan.kosa.cfd@gmail.com>
2020-10-11 23:18:44 +0200

    adding include guards

--- include/cuda.h
+++ include/cuda.h
@@ -1,2 +1,7 @@
+#ifndef CUDA_H_
+#define CUDA_H_
+
 #include <cuda_runtime_api.h> // just for proper indexing, nvcc includes it anyhow
 #define CUDA_HOSTDEV __host__ __device__
+
+#endif /* CUDA_H_ */
