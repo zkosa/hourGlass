be3235651785d49b8a955775a1ac711f2a1731d8
zkosa <zoltan.kosa.cfd@gmail.com>
2021-01-23 00:39:09 +0100

    extend CUDA checking macros

--- include/cuda.h
+++ include/cuda.h
@@ -1,11 +1,16 @@
 #ifndef CUDA_H_
 #define CUDA_H_
 
+#include <cstdio>
+#include <iostream>
+#include <cstdlib> // for exit
 #include <cuda_runtime_api.h> // just for proper indexing, nvcc includes it anyhow
+
+
 #define CUDA_HOSTDEV __host__ __device__
 #define CHECK_CUDA(cuda_call) { cudaAssert((cuda_call), __FILE__, __LINE__); }
-#define CHECK_CUDA_POST if (cudaPeekAtLastError() != cudaSuccess) printf("The error is %s", cudaGetErrorString(cudaGetLastError()));;
-
+#define CHECK_CUDA_POST { cudaAssert((cudaPeekAtLastError()), __FILE__, __LINE__); }
+#define CHECK_CUDA_POINTER(ptr) { cudaPointerAttributes attributes; printf("----\n"); printf(#ptr); printf(":\n"); CHECK_CUDA( cudaPointerGetAttributes(&attributes, ptr) ); cudaPrintPointerAttributes(attributes); }
 
 inline void cudaAssert(cudaError_t error_code, const char *file, int line, bool abort=true)
 {
@@ -18,5 +23,22 @@ inline void cudaAssert(cudaError_t error_code, const char *file, int line, bool 
 		}
 	}
 }
+
+inline void cudaPrintPointerAttributes(cudaPointerAttributes attributes) {
+	std::cout << "attributes.device:        " << attributes.device << std::endl;
+	std::cout << "attributes.devicePointer: " << attributes.devicePointer << std::endl;
+	std::cout << "attributes.hostPointer:   " << attributes.hostPointer << std::endl;
+	std::cout << "attributes.type:          " << attributes.type << std::endl;
+}
+
+/*
+template<class T>
+inline void CHECK_POINTER(T* ptr) {
+	cudaPointerAttributes* attributes = nullptr;
+	CHECK_CUDA( cudaPointerGetAttributes(attributes, ptr) );
+	cudaPrintPointerAttributes(attributes);
+}
+*/
+
 
 #endif /* CUDA_H_ */
