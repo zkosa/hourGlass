fa4647a7784f2c15c0919c65d71ebc655eff2155
zkosa <zoltan.kosa.cfd@gmail.com>
2021-01-23 22:53:02 +0100

    add usage help to function handler

--- include/functionhandler.h
+++ include/functionhandler.h
@@ -4,5 +4,10 @@
 // function handlers are used because std::function is not supported in CUDA
 template<typename T> using constFunctionHandler = float(T::*)(float) const;
 
+/* HOW TO CALL:
+ * constFunctionHandler<Boundary_axissymmetric> handler = boundaries_ax_ptr->functionHandler_contour;
+ * float res = (boundaries_ax_ptr->*handler)(0.0f);
+ */
+
 
 #endif /* INCLUDE_FUNCTIONHANDLER_H_ */
